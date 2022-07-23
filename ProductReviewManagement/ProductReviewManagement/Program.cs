06c9af0a80f3ce9e45a33181ac8ba23f7246178c
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-24 00:35:48 +0530

    [Shivaraj] Added DataTable for Product reviews

--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +16,11 @@ namespace ProductReviewManagementLINQ
             {
                 Console.WriteLine("Welcome To Product Review Management System\n");
                 List<ProductReview> list = ProductReviewManagement.AddProductsReview();
+                DataTable table = ProductReviewManagement.CreatingDataTable();
                 Console.WriteLine("Choose Option or press 0 for exit\n1:Add review to the list\n2:Top 3 high Rated product\n" +
                     "3:Products with Rating greater than 3\n4:Count of products for each ProductID\n5:Only ProductID with Review\n" +
-                    "6:Skipping top 5 records and displaying remaining records\n7:Only Retrieving ProductID with Rating");
+                    "6:Skipping top 5 records and displaying remaining records\n7:Only Retrieving ProductID with Rating\n" +
+                    "8:Create DataTable");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -45,6 +48,9 @@ namespace ProductReviewManagementLINQ
                     case 7:
                         ProductReviewManagement.RetrieveProductIDWithRating(list);
                         break;
+                    case 8:
+                        ProductReviewManagement.DisplayDataTable(table);
+                        break;
                 }
                 Console.ReadLine();
             }
@@ -57,9 +63,10 @@ namespace ProductReviewManagementLINQ
 }
 
 /*
-UC-7
+UC-8
 Product Review Management 
-- Retrieve only productId and review from the list for all records using LINQ select operator.
+- Create DataTable using C# and Add ProductID, UserID, Rating, Review and isLike fields in that.
+- Add 25 default values in datatable list which we have created.
 
 Welcome To Product Review Management System
 
@@ -71,18 +78,19 @@ Choose Option or press 0 for exit
 5:Only ProductID with Review
 6:Skipping top 5 records and displaying remaining records
 7:Only Retrieving ProductID with Rating
-7
-Only Retrieving ProductID with Rating
-ProductID: 1 Rating: 4.5
-ProductID: 4 Rating: 1.5
-ProductID: 5 Rating: 3.5
-ProductID: 8 Rating: 3.5
-ProductID: 1 Rating: 3.5
-ProductID: 7 Rating: 3
-ProductID: 9 Rating: 3.9
-ProductID: 3 Rating: 2.5
-ProductID: 6 Rating: 3.5
-ProductID: 2 Rating: 4.8
-ProductID: 7 Rating: 2
+8:Create DataTable
+8
+ProductId,  UserId,  Rating,  Review,  IsLike
+1,  34, Good,  True, 4.5
+3,  57, Good,  True, 3.9
+4,  56, Average,  True, 3
+7,  22, Bad,  False, 2
+8,  21, Good,  True, 4.7
+3,  67, Good,  True, 4.3
+2,  69, Good,  True, 4.4
+9,  13, Bad,  False, 1.5
+6,  81, Average,  True, 3.5
+5,  29, Good,  True, 4.9
+2,  30, Bad,  False, 2
 
  */
