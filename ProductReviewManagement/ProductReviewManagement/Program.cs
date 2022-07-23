cd11113254eb755d7f5408219912a757861d6f50
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-24 00:01:06 +0530

    [Shivaraj] Method to retrieve product with rating Greater Than 3 or Specific Products

--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -15,7 +15,8 @@ namespace ProductReviewManagementLINQ
             {
                 Console.WriteLine("Welcome To Product Review Management System\n");
                 List<ProductReview> list = ProductReviewManagement.AddProductsReview();
-                Console.WriteLine("Choose Option or press 0 for exit\n1:Add review to the list\n2:Top 3 high Rated product");
+                Console.WriteLine("Choose Option or press 0 for exit\n1:Add review to the list\n2:Top 3 high Rated product\n" +
+                    "3:Products with Rating greater than 3");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -27,6 +28,9 @@ namespace ProductReviewManagementLINQ
                         break;
                     case 2:
                         ProductReviewManagement.Top3HighRatedProduct(list);
+                        break;
+                    case 3:
+                        ProductReviewManagement.ProductsRatingGreaterThan3(list);
                         break;
                 }
                 Console.ReadLine();
@@ -40,18 +44,17 @@ namespace ProductReviewManagementLINQ
 }
 
 /*
-UC-2
-Product ReviewManagement
-- Retrieve top 3 records from the list who’s rating is high using LINQ
-
-Welcome To Product Review Management System
+UC-3
+Product Review Management 
+- Retrieve all record from the list who’s rating are greater then 3 and productID is 1 or 4 or 9 using
 
 Choose Option or press 0 for exit
 1:Add review to the list
 2:Top 3 high Rated product
-2
-Retrieving Top 3 products based on rating
-ProductID: 2, UserID: 24, Review: Good, IsLike: True, Rating: 4.8
+3:Products with Rating greater than 3
+3
+Retrieving products based on rating greater than 3 and having ProductID as 1/4/9
 ProductID: 1, UserID: 34, Review: Good, IsLike: True, Rating: 4.5
+ProductID: 1, UserID: 78, Review: Good, IsLike: True, Rating: 3.5
 ProductID: 9, UserID: 11, Review: Good, IsLike: True, Rating: 3.9
  */
