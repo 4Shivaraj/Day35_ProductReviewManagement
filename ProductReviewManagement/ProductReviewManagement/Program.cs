68724ce6381031ccc89c602bf51705da08f9d0c5
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-24 00:49:40 +0530

    [Shivaraj] Added Method to retrieve records with Nice Review

--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -20,7 +20,8 @@ namespace ProductReviewManagementLINQ
                 Console.WriteLine("Choose Option or press 0 for exit\n1:Add review to the list\n2:Top 3 high Rated product\n" +
                     "3:Products with Rating greater than 3\n4:Count of products for each ProductID\n5:Only ProductID with Review\n" +
                     "6:Skipping top 5 records and displaying remaining records\n7:Only Retrieving ProductID with Rating\n" +
-                    "8:Create DataTable\n9:Retrieve Records withIsLike value as True\n10:Find average Rating for all records");
+                    "8:Create DataTable\n9:Retrieve Records withIsLike value as True\n10:Find average Rating for all records\n" +
+                    "11: Find Records With Nice Review");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -57,6 +58,9 @@ namespace ProductReviewManagementLINQ
                     case 10:
                         ProductReviewManagement.FindAverageOfRating(table);
                         break;
+                    case 11:
+                        ProductReviewManagement.RecordsWithNiceReview(table);
+                        break;
                 }
                 Console.ReadLine();
             }
@@ -69,9 +73,9 @@ namespace ProductReviewManagementLINQ
 }
 
 /*
-UC-10
+UC-11
 Product Review Management 
-• Find average rating of the each productId using LINQ
+-Retreive all records from the list who’s review message contain "Nice" in it using LINQ.
 
 Welcome To Product Review Management System
 
@@ -86,15 +90,14 @@ Choose Option or press 0 for exit
 8:Create DataTable
 9:Retrieve Records withIsLike value as True
 10:Find average Rating for all records
-10
-Product Id: 1   Average Ratings: 4.5
-Product Id: 3   Average Ratings: 4.1
-Product Id: 4   Average Ratings: 3
-Product Id: 7   Average Ratings: 2
-Product Id: 8   Average Ratings: 4.7
-Product Id: 2   Average Ratings: 3.2
-Product Id: 9   Average Ratings: 1.5
-Product Id: 6   Average Ratings: 3.5
-Product Id: 5   Average Ratings: 4.9
+11: Find Records With Nice Review
+11
+ProductId, UserId, Review, IsLike, Rating
+1,  34,  Good,  True,  4.5
+3,  57,  Good,  True,  3.9
+8,  21,  Good,  True,  4.7
+3,  67,  Good,  True,  4.3
+2,  69,  Good,  True,  4.4
+5,  29,  Good,  True,  4.9
 
  */
