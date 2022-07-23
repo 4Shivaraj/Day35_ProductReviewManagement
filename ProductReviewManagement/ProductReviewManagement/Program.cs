91d2af5adf587237f9e87bb890b7bbb19380dd0d
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-24 00:44:56 +0530

    [Shivaraj] Added Method to find average rating for Product

--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -20,7 +20,7 @@ namespace ProductReviewManagementLINQ
                 Console.WriteLine("Choose Option or press 0 for exit\n1:Add review to the list\n2:Top 3 high Rated product\n" +
                     "3:Products with Rating greater than 3\n4:Count of products for each ProductID\n5:Only ProductID with Review\n" +
                     "6:Skipping top 5 records and displaying remaining records\n7:Only Retrieving ProductID with Rating\n" +
-                    "8:Create DataTable\n9:Retrieve Records withIsLike value as True");
+                    "8:Create DataTable\n9:Retrieve Records withIsLike value as True\n10:Find average Rating for all records");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -54,6 +54,9 @@ namespace ProductReviewManagementLINQ
                     case 9:
                         ProductReviewManagement.RetriveRecordsWithIsLikeTrue(table);
                         break;
+                    case 10:
+                        ProductReviewManagement.FindAverageOfRating(table);
+                        break;
                 }
                 Console.ReadLine();
             }
@@ -66,9 +69,9 @@ namespace ProductReviewManagementLINQ
 }
 
 /*
-UC-9
+UC-10
 Product Review Management 
-- Retrieve all the records from the datatable variable who’s isLike value is true using LINQ
+• Find average rating of the each productId using LINQ
 
 Welcome To Product Review Management System
 
@@ -82,16 +85,16 @@ Choose Option or press 0 for exit
 7:Only Retrieving ProductID with Rating
 8:Create DataTable
 9:Retrieve Records withIsLike value as True
-9
-Displaying reviews with IsLike value as True
-ProductId, UserId, Review, IsLike, Rating
-1,  34,  Good,  True,  4.5
-3,  57,  Good,  True,  3.9
-4,  56,  Average,  True,  3
-8,  21,  Good,  True,  4.7
-3,  67,  Good,  True,  4.3
-2,  69,  Good,  True,  4.4
-6,  81,  Average,  True,  3.5
-5,  29,  Good,  True,  4.9
+10:Find average Rating for all records
+10
+Product Id: 1   Average Ratings: 4.5
+Product Id: 3   Average Ratings: 4.1
+Product Id: 4   Average Ratings: 3
+Product Id: 7   Average Ratings: 2
+Product Id: 8   Average Ratings: 4.7
+Product Id: 2   Average Ratings: 3.2
+Product Id: 9   Average Ratings: 1.5
+Product Id: 6   Average Ratings: 3.5
+Product Id: 5   Average Ratings: 4.9
 
  */
