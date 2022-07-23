a54482c889bab3183232d064d28ea9746f58d26e
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-23 23:44:45 +0530

    [Shivaraj] Added Creating product review list

--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,31 +11,47 @@ namespace ProductReviewManagementLINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Product Review Management System");
-            List<ProductReview> list = ProductReviewManagement.AddProductsReview();
-            ProductReviewManagement.DisplayeProductsReview(list);
-            Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Welcome To Product Review Management System\n");
+                List<ProductReview> list = ProductReviewManagement.AddProductsReview();
+                Console.WriteLine("Choose Option or press 0 for exit\n1:Add review to the list\n2:Top 3 high Rated product");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        ProductReviewManagement.DisplayeProductsReview(list);
+                        break;
+                    case 2:
+                        ProductReviewManagement.Top3HighRatedProduct(list);
+                        break;
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
 
 /*
-UC-1
-Product Review Management
-- Create Product review class with ProductID, UserID, Rating, Review and isLike fields
-- Create variable for List of Product Review class in Main method
-- Add 25 default values in list which we have created
+UC-2
+Product ReviewManagement
+- Retrieve top 3 records from the list who’s rating is high using LINQ
 
- * Welcome To Product Review Management System
-ProductID: 1, UserID: 34, Review: Good, IsLike: True, Rating: 4.5
-ProductID: 4, UserID: 23, Review: Bad, IsLike: False, Rating: 1.5
-ProductID: 5, UserID: 39, Review: Good, IsLike: True, Rating: 3.5
-ProductID: 8, UserID: 57, Review: Average, IsLike: True, Rating: 3.5
-ProductID: 1, UserID: 78, Review: Good, IsLike: True, Rating: 3.5
-ProductID: 7, UserID: 22, Review: Average, IsLike: True, Rating: 3
-ProductID: 9, UserID: 11, Review: Good, IsLike: True, Rating: 3.9
-ProductID: 3, UserID: 45, Review: Bad, IsLike: False, Rating: 2.5
-ProductID: 6, UserID: 89, Review: Good, IsLike: True, Rating: 3.5
+Welcome To Product Review Management System
+
+Choose Option or press 0 for exit
+1:Add review to the list
+2:Top 3 high Rated product
+2
+Retrieving Top 3 products based on rating
 ProductID: 2, UserID: 24, Review: Good, IsLike: True, Rating: 4.8
-ProductID: 7, UserID: 10, Review: Bad, IsLike: False, Rating: 2
+ProductID: 1, UserID: 34, Review: Good, IsLike: True, Rating: 4.5
+ProductID: 9, UserID: 11, Review: Good, IsLike: True, Rating: 3.9
  */
